f5cdd31d42ad7762a2ac1236b32ba2acf211a743
mortiz407 <33585811+mortiz407@users.noreply.github.com>
2026-08-09 00:53:17 -0500

    Implement product catalog display
    This will display what I have in my catalog database

--- Ortiz_PHP.php
+++ Ortiz_PHP.php
@@ -5,7 +5,20 @@
   </head>
 
   <body>
-    <from method='POST'>
-      <h3>
+    <h1 style="text-align: center;">Product Catalog</h1>
+
+    <?php if (count($products) > 0):
+      foreach ($products as $product):
+    ?>
+        <div>
+          <h3>
+            ID: <?= $product['ProductID']?>  | <?= htmlspecialchars($product['ProductName'])?> | <?= number_format($product['ProductCost'], 2)?>
+          </h3>
+          <p><?= htmlspecialchars($product['ProductDescription'])?></p>
+        </div>
+    <?php endforeach;
+      else: ?>
+        <p>No product was found in catalog</p>
+    <?php endif; ?>
   </body>
 </html>
