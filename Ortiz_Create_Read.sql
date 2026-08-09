077e03b350419d38eb181ea9e18de95e8bb7fbed
mortiz407 <33585811+mortiz407@users.noreply.github.com>
2026-08-08 23:28:51 -0500

    updated sql code
    Reinserted catalog and cart data along with creating the cart table. Added SELECT statements to read from both catalog and cart.

--- Ortiz_Create_Read.sql
+++ Ortiz_Create_Read.sql
@@ -1,4 +1,4 @@
---Create Catalog page
+--Create Catalog page database
 INSERT INTO catalog (ProductID, ProductName,ProductDescription, ProductCost, Quantity) VALUES
 (1, 'Strawberry Lemonade', 'Handcrafted lemonade with fresh Strawberries', 4.00, 0),
 (2, 'Blueberries Lemonade', 'Handcrafted lemonade with fresh Blueberries', 4.00, 0),
@@ -6,18 +6,20 @@ INSERT INTO catalog (ProductID, ProductName,ProductDescription, ProductCost, Qua
 (4, 'Pinapples Lemonade', 'Handcrafted lemonade with fresh Pinapples', 4.00, 0),
 (5, 'Mangos Lemonade', 'Handcrafted lemonade with fresh Mangos', 4.00, 0);
 
---Create Shopping cart page
-INSERT INTO cart (ProductID, ProductName,QuantityOrdered, ProductCost, ProductTotal) VALUES
-(1, 'Strawberry Lemonade', 0,4.00, 0),
-(2, 'Blueberries Lemonade', 0,4.00, 0),
-(3, 'Watermelon Lemonade', 0,4.00, 0),
-(4, 'Pinapples Lemonade', 0,4.00, 0),
-(5, 'Mangos Lemonade', 0,4.00, 0);
+--Create Shopping cart page table 
+CREATE TABLE cart (
+  ProductID VARCHAR(50) NOT NULL,
+  ProductName VARCHAR(100) NOT NULL,
+  QuantityOrder INT NOT NULL,
+  ProductCost DECIMAL(10,2) NOT NULL,
+  ProductTotal DECIMAL(10,2) NOT NULL,
+  PRIMARY KEY (ProductID)
+  );
 
 --Read catalog
 SELECT *
 FROM catalog;
 
---Read cart
+--Read and check if table was created
 SELECT *
 FROM cart;
