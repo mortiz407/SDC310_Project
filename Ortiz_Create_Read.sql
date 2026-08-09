a1c4437b9365448f9b5a645f11a8049df664235c
mortiz407 <33585811+mortiz407@users.noreply.github.com>
2026-08-09 01:21:55 -0500

    Update product quantities in catalog and SQL structure

--- Ortiz_Create_Read.sql
+++ Ortiz_Create_Read.sql
@@ -1,10 +1,10 @@
 --Create Catalog page database
 INSERT INTO catalog (ProductID, ProductName,ProductDescription, ProductCost, Quantity) VALUES
-(1, 'Strawberry Lemonade', 'Handcrafted lemonade with fresh Strawberries', 4.00, 0),
-(2, 'Blueberries Lemonade', 'Handcrafted lemonade with fresh Blueberries', 4.00, 0),
-(3, 'Watermelon Lemonade', 'Handcrafted lemonade with fresh Watermelon', 4.00, 0),
-(4, 'Pinapples Lemonade', 'Handcrafted lemonade with fresh Pinapples', 4.00, 0),
-(5, 'Mangos Lemonade', 'Handcrafted lemonade with fresh Mangos', 4.00, 0);
+(1, 'Strawberry Lemonade', 'Handcrafted lemonade with fresh Strawberries', 4.00, 10),
+(2, 'Blueberries Lemonade', 'Handcrafted lemonade with fresh Blueberries', 4.00, 20),
+(3, 'Watermelon Lemonade', 'Handcrafted lemonade with fresh Watermelon', 4.00, 15),
+(4, 'Pinapples Lemonade', 'Handcrafted lemonade with fresh Pinapples', 4.00, 5),
+(5, 'Mangos Lemonade', 'Handcrafted lemonade with fresh Mangos', 4.00, 7);
 
 --Create Shopping cart page table 
 CREATE TABLE cart (
